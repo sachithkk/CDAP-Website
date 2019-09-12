9ae3b2d8716d7b6d14875fbca256c5615f74e686
Sajith Sudharshana <ssgunathilaka@gmail.com>
2019-09-12 14:03:30 +0530

    Modified

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     $().UItoTop({
         easingType: 'easeOutQuart'
     });
@@ -28,4 +28,33 @@ function myMap() {
         zoom: 15,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function a() {
+    var recipient="virajlakshitha39";
+    var at = String.fromCharCode(64);
+    var dotcom="gmail.com";
+    var mail="mailto:";
+    window.open(mail+recipient+at+dotcom);
 }
